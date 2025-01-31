b50dce0b4490ca2bec009abd031ceec490e3933e
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2025-01-31 22:01:05 +0530

    first and second section responsive for i pad lil ass but works more detail like adding img top will do later

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import FirstSection from "./costomcomp/FirstSection";
+import SecondSection from "./costomcomp/SecondSection";
 import Navbar from "./costomcomp/Navbar";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <FirstSection />
+      <SecondSection />
     </div>
   );
 }
