737258a2b8c3ee54e94cdb352551bb2492ac919f
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-01-31 22:24:53 +0530

    tagline

--- app/costomcomp/Navbar.js
+++ app/costomcomp/Navbar.js
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <>
       <div>
-        <div className=" bg-gradient-to-b from-cyan-500 to-cyan-50  w-full h-48 flex items-center justify-center text-white font-bold">
+        <div className="bg-cyan-500 w-full h-48 flex items-center justify-center text-white font-bold">
           <h1 className="text-6xl drop-shadow-lg">Liwa </h1>
         </div>
-        <div className="flex items-end justify-end pr-10">
+        <div className="flex items-end justify-end sm:pr-10">
           <Link
             href="/#"
-            className="pr-3 font-bold text-blue-500 hover:text-blue-700 hover:underline transition duration-300"
+            className="pr-3 font-bold text-blue-500  hover:text-blue-700 hover:underline transition duration-300"
           >
             About Us
           </Link>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Tagline from "./costomcomp/Tagline";
 import Navbar from "./costomcomp/Navbar";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Tagline />
     </div>
   );
 }
