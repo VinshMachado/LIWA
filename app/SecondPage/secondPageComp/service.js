8ed57267cad78cfc68470cb33db11aa3e913de8c
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2025-02-01 22:27:40 +0530

    added some style to second page

--- app/SecondPage/secondPageComp/service.js
+++ app/SecondPage/secondPageComp/service.js
@@ -20,20 +20,18 @@ const service = () => {
           Our Services:
         </span>
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5">
             <p>
               <MdOutlineDryCleaning size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl  font-serif text-pink-500 font-semibold ld ">
                 {" "}
                 1) Dry Cleaning:
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
-                At our laundry service, we take pride in caring for your
-                clothes, leaving them fresh, pressed, and as good as new. Our
-                fast, reliable, and eco-friendly service makes laundry day
-                effortless, so you can enjoy more free time while we handle the
-                work.
+              <span className="text-xl font-mono text-blue-500  font-medium ">
+                At our laundry service, we ensure your clothes are fresh,
+                pressed, and like new. Our fast, reliable, and eco-friendly
+                service makes laundry day effortless, giving you more free time.
               </span>
             </p>
           </div>
@@ -43,21 +41,19 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400  border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiWashingMachine size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 2) Washing Services
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
-                We offer professional washing services for all types of clothing
-                and fabrics. Whether it's delicate garments or everyday wear, we
-                ensure your items are cleaned with the utmost care. Our
-                eco-friendly process guarantees freshness and quality every
-                time. Trust us to handle your laundry with precision and
-                expertise.
+              <span className="text-xl text-blue-500 font-mono font-medium ">
+                We offer professional washing for all types of clothing and
+                fabrics, from delicate items to everyday wear. Our eco-friendly
+                process ensures freshness and quality every time. Trust us to
+                handle your laundry with care and expertise.
               </span>
             </p>
           </div>
@@ -67,15 +63,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <TbIroningSteamFilled size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 3) Steam Ironing
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 Experience fast and efficient steam ironing to remove wrinkles
                 and refresh your clothes. We ensure your garments look crisp and
                 polished. Perfect for a smooth, wrinkle-free finish every time!
@@ -89,15 +85,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <FaRug size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 4) Carpet Cleaning
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We offer deep cleaning and effective stain removal for all types
                 of carpets and rugs. Our expert service restores freshness and
                 vibrancy to your floors. Trust us to tackle even the toughest
@@ -112,15 +108,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiTheaterCurtains size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 5) Curtain Cleaning
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We provide thorough cleaning for curtains, drapes, and blinds,
                 ensuring they’re fresh, dust-free, and looking like new. Our
                 service restores their beauty and extends their lifespan.
@@ -134,15 +130,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiSofa size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 6) Sofa Cleaning
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We offer professional upholstery cleaning to maintain the
                 cleanliness and appearance of your sofas and chairs. Our service
                 removes dirt and stains, keeping your furniture looking fresh
@@ -157,15 +153,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <FaMattressPillow size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 7)Mattress Shampooing
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We provide deep cleaning and refreshing for your mattress,
                 removing dust, allergens, and stains. Our service ensures a
                 cleaner, healthier sleep environment for you.
@@ -179,15 +175,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiHanger size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 8)Mending & Alteration Services
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We offer professional mending, repairs, and alterations for all
                 your clothing and garments. Our expert service ensures a perfect
                 fit and extends the life of your wardrobe.
@@ -200,15 +196,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <FaUserTie size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 9)Wedding Clothes Cleaning
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We provide special care cleaning for wedding gowns and bridal
                 wear to preserve their beauty. Our delicate process ensures your
                 dress remains pristine and ready for treasured memories.
@@ -222,15 +218,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiRunningShoe size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 10) Shoes Cleaning & Restoration
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We offer cleaning, polishing, and restoration services for your
                 shoes to bring them back to life. Our expert care restores their
                 shine and extends their durability.
@@ -244,15 +240,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <GiSchoolBag size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 11) Bag Cleaning & Restoration
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We provide cleaning and restoration services for bags, from
                 leather to fabric, keeping them in great shape. Our expert care
                 preserves their quality and extends their lifespan.
@@ -266,15 +262,15 @@ const service = () => {
 
       <div className="mt-16">
         <div className="flex justify-center ">
-          <div className="border-4 border-solid border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
+          <div className="border-4 border-solid shadow-xl hover:-translate-y-4  duration-300 ease-in-out shadow-pink-400 border-pink-500 w-8/12 rounded-xl sm:h-[400px] md:h-[390px] lg:h-[320px] mt-3 p-5 mb-3">
             <p>
               <TbHorseToy size={160} />
-              <span className="text-2xl font-medium ">
+              <span className="text-2xl font-serif text-pink-500 font-semibold  ">
                 {" "}
                 12) Soft Toys Cleaning
               </span>{" "}
               <br />
-              <span className="text-xl font-normal ">
+              <span className="text-xl text-blue-500 font-mono font-medium ">
                 We provide safe and gentle cleaning of soft toys, ensuring
                 they’re hygienic and kid-friendly. Our care keeps them soft,
                 clean, and safe for children to enjoy.
