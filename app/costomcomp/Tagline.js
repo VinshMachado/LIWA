19ee56372ff6a292f5a7eec6c930b461156be492
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-02-02 14:56:07 +0530

    carocel

--- app/costomcomp/Tagline.js
+++ app/costomcomp/Tagline.js
@@ -11,26 +11,41 @@ const Tagline = () => {
   return (
     <div className="relative w-full h-[400px] ">
       <div className="bg-[url('/tagline.jpeg')] bg-cover bg-center w-full h-full  absolute inset-0 flex items-center justify-center flex-wrap ">
-        <div className="absolute inset-0 bg-gradient-to-t from-transparent via-white/50 to-white" />
+        <div className="absolute inset-0 bg-gradient-to-t from-transparent  to-white" />
         <Carousel opts={{ align: "center", loop: true }}>
           <CarouselContent className="gap-4">
             <CarouselItem className="flex justify-center">
-              <img
-                src="/cloths.png"
-                className="sm:w-auto sm:h-96 w-70 h-70  "
-              />
+              <div className=" items-center justify-center flex flex-wrap">
+                <img
+                  src="/cloths.png"
+                  className="sm:w-auto sm:h-96 w-70 h-56  "
+                />
+                <h1 className="text-4xl font-bold text-pink-500 drop-shadow-xl">
+                  Cloths
+                </h1>
+              </div>
             </CarouselItem>
             <CarouselItem className="flex justify-center">
-              <img
-                src="/pngtree-contemporary-linen-sofa-on-white-studio-background-a-png-image_10294836.png"
-                className="sm:w-96 sm:h-96 w-70 h-auto   "
-              />
+              <div className=" items-center justify-center flex flex-wrap">
+                <img
+                  src="/pngtree-contemporary-linen-sofa-on-white-studio-background-a-png-image_10294836.png"
+                  className="sm:w-96 sm:h-96 h-96 w-96   "
+                />
+                <h1 className="text-4xl font-bold text-pink-500 drop-shadow-xl">
+                  Sofa's
+                </h1>
+              </div>
             </CarouselItem>
             <CarouselItem className="flex justify-center">
-              <img src="/matres.png" className=" sm:h-72 w-auto h-72 mt-7  " />
-            </CarouselItem>
-            <CarouselItem className="flex justify-center">
-              <img src="/toys.png" className=" sm:h-72 w-auto h-64 pt-20 " />
+              <div className=" items-center justify-center flex flex-wrap">
+                <img
+                  src="/matres.png"
+                  className=" sm:w-72 h-auto w-36 mt-7  "
+                />
+                <h1 className="text-4xl ml-5 font-bold text-pink-500 drop-shadow-xl">
+                  Mattress
+                </h1>
+              </div>
             </CarouselItem>
           </CarouselContent>
         </Carousel>
