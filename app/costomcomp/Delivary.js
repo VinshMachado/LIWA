6bf46daadc442c244ae71d02b31f25e9704cced4
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-02-02 16:20:48 +0530

    truck

--- app/costomcomp/Delivary.js
+++ app/costomcomp/Delivary.js
@@ -3,7 +3,7 @@ import React from "react";
 const Delivary = () => {
   return (
     <div className="mt-28">
-      <div className="flex flex-col sm:flex-row sm:pl-24 pl-20 pr-24 ">
+      <div className="flex flex-col sm:flex-row sm:pl-24 pl-20 pr-24 justify-center items-center">
         {/* Text Section */}
         <div className="flex flex-col text-center">
           <p className="text-black text-xl sm:text-3xl drop-shadow-2xl">
@@ -20,7 +20,7 @@ const Delivary = () => {
         <img
           src="/Truck.png"
           alt="Tagline"
-          className="w-28 h-auto sm:ml-20  sm:w-48 pb-10 ml-8"
+          className="w-28 h-auto sm:ml-20  sm:w-48 pb-10 ml-15"
         />
       </div>
     </div>
