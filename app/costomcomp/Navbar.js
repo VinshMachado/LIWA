dc00cac4d5d7f72133244fdbe379df64f9eb4a5b
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-02-02 14:23:47 +0530

    nav bar

--- app/costomcomp/Navbar.js
+++ app/costomcomp/Navbar.js
@@ -5,10 +5,15 @@ const Navbar = () => {
   return (
     <>
       <div>
-        <div className="bg-white bg-cover bg-center  w-full h-48 flex items-center justify-center text-white font-bold flex-col">
-          <img src="/logo.png" className="h-36 w-auto  drop-shadow-lg" />
+        <div className="relative bg-[url('/yoyo.jpeg')] bg-cover bg-center w-full h-48 flex items-center justify-center text-white font-bold flex-col">
+          {/* Gradient Layer */}
+
+          {/* Content */}
+          <div className="relative z-10 flex flex-col items-center border-">
+            <img src="/logo.png" className="h-36 w-auto drop-shadow-lg" />
+          </div>
         </div>
-        <div className="flex items-end justify-end sm:pr-10">
+        <div className="flex items-end justify-end sm:pr-10 ">
           <Link
             href="/#"
             className="pr-3 font-bold text-blue-500  hover:text-blue-700 hover:underline transition duration-300 hover:-translate-y-1 "
