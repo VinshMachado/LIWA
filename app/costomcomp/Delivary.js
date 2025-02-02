59fc4fc5e5f3ed36a5d9804ec15aa9039d13ed01
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-02-02 16:33:00 +0530

    van ka new version

--- app/costomcomp/Delivary.js
+++ app/costomcomp/Delivary.js
@@ -6,7 +6,7 @@ const Delivary = () => {
       <div className="flex flex-col sm:flex-row sm:pl-24 pl-20 pr-24 justify-center items-center">
         {/* Text Section */}
         <div className="flex flex-col text-center">
-          <p className="text-black text-xl sm:text-3xl drop-shadow-2xl">
+          <p className="text-black w-full  text-xl sm:text-3xl drop-shadow-2xl">
             <b>Experience Laundry Like Never Before </b>
             with
             <b> Free Pickup and Delivery </b>
@@ -18,9 +18,9 @@ const Delivary = () => {
 
         {/* Image */}
         <img
-          src="/Truck.png"
+          src="/van.jpeg"
           alt="Tagline"
-          className="w-28 h-auto sm:ml-20  sm:w-48 pb-10 ml-15"
+          className="w-48 h-auto sm:ml-20  sm:w-48 pb-10 ml-15"
         />
       </div>
     </div>
