e1a0ab2e3b3f3eab8eef8ffc121035abd6b2b0bc
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-02-06 23:02:05 +0530

    bg change

--- app/SecondPage/page.js
+++ app/SecondPage/page.js
@@ -3,7 +3,7 @@ import Navbar from "../costomcomp/Navbar";
 import Service from "./secondPageComp/service";
 const page = () => {
   return (
-    <div className="bg-[url('/bgedit.png')] bg-cover bg-center">
+    <div className="bg-[url('/servicesbg.jpg')] bg-cover bg-center">
       <Service />
     </div>
   );
